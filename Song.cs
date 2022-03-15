649d12bd9d561e06654ba0b7b792891877c1fd5b
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-03-15 22:53:34 +0300

    Добавьте файлы проекта.

---
+++ Song.cs
@@ -0,0 +1,55 @@
+﻿namespace LabWorkWithSongs
+{
+    public class Song
+    {
+        private readonly string _name;
+        private readonly string _author;
+
+        public Song PastSong
+        {
+            get { return PastSong; }
+            set
+            {
+                Equals(value);
+                PastSong = value;
+            }
+        }
+
+        public Song(string name, string author)
+        {
+            _name = name;
+            _author = author;
+        }
+
+
+        public void PrintSong()
+        {
+            Console.WriteLine(Title());
+        }
+
+        public string Title() => String.Format($"{_author} - {_name}");
+
+        public bool Equals(Song? song)
+        {
+            return song != null && song._author == _author && song._name == _name;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{_author} - {_name}");
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            return Equals(obj as Song);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_author, _name);
+        }
+    }
+}
